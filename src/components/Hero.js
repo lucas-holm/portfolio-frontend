a7659e382421f7adbafb02ab33891aee9768328e
lucas-holm <55479586+lucas-holm@users.noreply.github.com>
2020-09-29 07:48:11 +0200

    init

---
+++ src/components/Hero.js
@@ -0,0 +1,49 @@
+import React from "react"
+import Image from "gatsby-image"
+import { Link } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
+import SocialLinks from "../constants/socialLinks"
+// ...GatsbyImageSharpFluid
+
+const query = graphql`
+  {
+    file(relativePath: { eq: "hero-img.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
+const Hero = () => {
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query)
+
+  // console.log(data)
+
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <h1>I'm Lucas</h1>
+            <div className="underline"></div>
+            <h2>Aa hallå röh</h2>
+            <Link to="/contact" className="btn">
+              kontakta mig
+            </Link>
+            <SocialLinks />
+          </div>
+        </article>
+        <Image fluid={fluid} className="hero-img" />
+      </div>
+    </header>
+  )
+}
+
+export default Hero
